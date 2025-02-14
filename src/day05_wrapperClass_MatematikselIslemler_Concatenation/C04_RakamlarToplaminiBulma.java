e9997178df0e4d0301f2dd76ddf8b486e10b836c
bulutluoz <abbulutluoz@gmail.com>
2025-02-14 20:16:30 +0100

    5.gun sonu

---
+++ src/day05_wrapperClass_MatematikselIslemler_Concatenation/C04_RakamlarToplaminiBulma.java
@@ -0,0 +1,41 @@
+package day05_wrapperClass_MatematikselIslemler_Concatenation;
+
+import java.util.Scanner;
+
+public class C04_RakamlarToplaminiBulma {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan 3 basamakli pozitif bir tamsayi alip
+        // girilen sayinin rakamlar toplamini yazdirin
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen 3 basamakli pozitif bir tamsayi giriniz...");
+        int sayi = scanner.nextInt();  // 234
+
+        int rakam = 0;
+        int rakamlarToplami = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
