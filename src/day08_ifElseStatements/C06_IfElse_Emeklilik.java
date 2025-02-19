19ec2e92662bb4d5b1fbdfc5e7bc87e00a341133
bulutluoz <abbulutluoz@gmail.com>
2025-02-19 20:18:51 +0100

    8.ders sonu

---
+++ src/day08_ifElseStatements/C06_IfElse_Emeklilik.java
@@ -0,0 +1,10 @@
+package day08_ifElseStatements;
+
+public class C06_IfElse_Emeklilik {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan yasini isteyin, 65 yas ve uzeri ise ”Emekli olabilirsin” yazdirin,
+        // yoksa emekli olmasi icin calismasi gereken yil sayisini yazdirin.
+    }
+}
