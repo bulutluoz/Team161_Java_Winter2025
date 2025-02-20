54963975f65e226042e50dd982d738768c6c0d78
bulutluoz <abbulutluoz@gmail.com>
2025-02-20 19:17:10 +0100

    9.ders sonu

---
+++ src/day09_ifElseIfStatements_nestedIfElse/C03_IndirimliFiyatHesaplama.java
@@ -0,0 +1,16 @@
+package day09_ifElseIfStatements_nestedIfElse;
+
+public class C03_IndirimliFiyatHesaplama {
+
+    public static void main(String[] args) {
+
+        // Kullaniciya kac adet urun aldigini ve bir urunun fiyatini sorun
+        // eger kullanici 100 adet veya daha fazla aldiysa %25,
+        // eger 50 adet veya daha fazla aldiysa %20
+        // eger 10 adet veya daha fazla aldiysa %10 indirim yaparak
+        // odeyecegi toplam miktari yazdirin
+        // Kullanici negatif bir fiyat veya adet girerse uyari verin
+
+
+    }
+}
