edf08e2bb16b561f4a97a89d158872406d4b62e1
bulutluoz <abbulutluoz@gmail.com>
2025-02-20 20:01:03 +0100

    9.ders sonu

--- src/day09_ifElseIfStatements_nestedIfElse/C03_IndirimliFiyatHesaplama.java
+++ src/day09_ifElseIfStatements_nestedIfElse/C03_IndirimliFiyatHesaplama.java
@@ -1,5 +1,7 @@
 package day09_ifElseIfStatements_nestedIfElse;
 
+import java.util.Scanner;
+
 public class C03_IndirimliFiyatHesaplama {
 
     public static void main(String[] args) {
@@ -10,6 +12,23 @@ public class C03_IndirimliFiyatHesaplama {
         // eger 10 adet veya daha fazla aldiysa %10 indirim yaparak
         // odeyecegi toplam miktari yazdirin
         // Kullanici negatif bir fiyat veya adet girerse uyari verin
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Aldiginiz urun adedini giriniz...");
+        int adet = scanner.nextInt();
+
+        System.out.println("Aldiginiz urunun birim fiyatini giriniz...");
+        double birimFiyati = scanner.nextDouble();
+
+        double indirimsizToplamFiyat = adet * birimFiyati ;
+
+        if (adet >= 100) System.out.println("%25 indirimli toplam fiyat : " + indirimsizToplamFiyat * 75 /100);
+        else if (adet >= 50) System.out.println("%20 indirimli toplam fiyat : " + indirimsizToplamFiyat * 80 /100);
+        else if (adet >= 10) System.out.println("%10 indirimli toplam fiyat : " + indirimsizToplamFiyat * 90 /100);
+        else if (adet >= 0) System.out.println("Indirimsiz toplam fiyat : " + indirimsizToplamFiyat);
+        else System.out.println("Girilen adet hatali");
+
 
 
     }
