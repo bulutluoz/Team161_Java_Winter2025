8a4404378073293a005e449cbc2222722c7b8c2f
bulutluoz <abbulutluoz@gmail.com>
2025-02-24 19:34:12 +0100

    Konu11 sonu

---
+++ src/day11_switchStatement_stringManipulations/C04_YuzdenKucukPozitifSayiYazdirma.java
@@ -0,0 +1,16 @@
+package day11_switchStatement_stringManipulations;
+
+public class C04_YuzdenKucukPozitifSayiYazdirma {
+
+    public static void main(String[] args) {
+
+        // kullanicidan yuzden kucuk pozitif bir tamsayi isteyin
+        // girilen sayiyi yazi ile yazdirin
+        // orn input : 17 , output : Onyedi
+        // 0-99 arasindaki sayilar disinda sayi yazarsa hata mesaji verin
+
+
+
+    }
+
+}
