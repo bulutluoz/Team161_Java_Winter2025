bd830a151568e05974484a40eae3c48f5855bdf9
bulutluoz <abbulutluoz@gmail.com>
2025-02-26 19:16:31 +0100

    Day13 sonu

---
+++ src/day13_stringManipulations/C03_Soru.java
@@ -0,0 +1,30 @@
+package day13_stringManipulations;
+
+import java.util.Scanner;
+
+public class C03_Soru {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan bir cumle ve cumlede aratilacak bir metin isteyin
+        // girilen cumle ve metin'e gore asagidaki 3 sonucdan uygun olani yazdirin
+        // 1- cumle aranan metni icermiyor
+        // 2- cumle aranan metni sadece 1 adet iceriyor
+        // 3- cumle aranan metni 1'den fazla iceriyor
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen bir cumle giriniz...");
+        String cumle = scanner.nextLine();
+
+
+        System.out.println("Cumlede aranacak metni giriniz...");
+        String metin = scanner.nextLine();
+
+
+
+
+
+
+
+    }
+}
