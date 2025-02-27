cf07524b4ca4c0418da04ea9c3e99ac578d640aa
bulutluoz <abbulutluoz@gmail.com>
2025-02-27 20:38:44 +0100

    Ders14 sonu

--- src/day14_stringmanipulations/C06_IsmiDegistirme.java
+++ src/day14_stringmanipulations/C06_IsmiDegistirme.java
@@ -20,5 +20,34 @@ public class C06_IsmiDegistirme {
         System.out.println("Lutfen soyisminizi giriniz...");
         String soyisim = scanner.nextLine();
 
+        if ( isim.length() > soyisim.length()){
+            // isim daha uzunsa
+            //    isim ve soyismi ilk harf buyuk kalanlar kucuk seklinde yazdirin
+
+            System.out.println(
+
+                    isim.substring(0,1).toUpperCase()+
+                    isim.substring(1).toLowerCase()+
+                    " "+
+                    soyisim.substring(0,1).toUpperCase()+
+                    soyisim.substring(1).toLowerCase()
+            );
+
+
+
+        } else if (soyisim.length() > isim.length()) {
+            // soyisim daha uzunsa
+            //    ismi ilk harf buyuk digerleri kucuk, soyismin tamamini buyuk harflerle yazdirin.
+
+            System.out.println(
+                    isim.substring(0,1).toUpperCase()+
+                    isim.substring(1).toLowerCase()+
+                    " "+
+                    soyisim.toUpperCase()
+            );
+
+        }
+
+
     }
 }
