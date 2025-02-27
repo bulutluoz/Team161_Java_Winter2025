ab47591e2ec189205c8c66b7db3e8f21ba0bea8a
bulutluoz <abbulutluoz@gmail.com>
2025-02-27 20:14:28 +0100

    Ders14 sonu

---
+++ src/day14_stringmanipulations/C06_IsmiDegistirme.java
@@ -0,0 +1,24 @@
+package day14_stringmanipulations;
+
+import java.util.Scanner;
+
+public class C06_IsmiDegistirme {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan isim ve soyismini ayri ayri alin.
+        //	- ismi daha uzun ise,
+        //    isim ve soyismi ilk harf buyuk kalanlar kucuk seklinde yazdirin
+        //  - soyisim daha uzun ise,
+        //    ismi ilk harf buyuk digerleri kucuk, soyismin tamamini buyuk harflerle yazdirin.
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lutfen isminizi giriniz...");
+        String isim = scanner.nextLine();
+
+        System.out.println("Lutfen soyisminizi giriniz...");
+        String soyisim = scanner.nextLine();
+
+    }
+}
