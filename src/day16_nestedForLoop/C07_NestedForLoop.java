3afa02145ec5ac0281aa8d3c8c7a5818a713345f
bulutluoz <abbulutluoz@gmail.com>
2025-03-03 21:16:12 +0100

    Ders16 sonu

--- src/day16_nestedForLoop/C07_NestedForLoop.java
+++ src/day16_nestedForLoop/C07_NestedForLoop.java
@@ -2,20 +2,21 @@ package day16_nestedForLoop;
 
 import java.util.Scanner;
 
-public class C06_NestedForLoop {
+public class C07_NestedForLoop {
 
-            /*
-            kullanicidan satir ve sutun sayisini alip asagidaki sekli yazdirin
-            orn satir = 4 , sutun= 5 oldugunda
-             1 2 3 4 5
-             2 3 4 5 6
-             3 4 5 6 7
-             4 5 6 7 8
+    public static void main(String[] args) {
+
+        /*
+
+        kullanicidan satir ve sutun sayisini alip asagidaki sekli cizdirin
+        orn satir = 3 , sutun= 6 oldugunda
+
+                * * * * * *
+                * * * * * *
+                * * * * * *
 
          */
 
-
-    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Lutfen sekildeki satir sayisini giriniz...");
         int satirSayisi = scanner.nextInt();
@@ -24,10 +25,12 @@ public class C06_NestedForLoop {
         int satirdakiElemanSayisi = scanner.nextInt();
 
         for (int i = 1; i <=satirSayisi ; i++) { // satirlar
-            for (int j = 1; j <=satirdakiElemanSayisi ; j++) { // her satirdaki elemanlar
 
-                System.out.print( i+j-1 + " "   );
+            for (int j = 1; j <=satirdakiElemanSayisi ; j++) { // her satirdaki eleman sayisi
+
+                System.out.print("* " );
             }
+
             System.out.println();
         }
 
