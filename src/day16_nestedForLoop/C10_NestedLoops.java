e832af638a778840db66242d155f9c466ea3b376
bulutluoz <abbulutluoz@gmail.com>
2025-03-04 19:41:38 +0100

    Ders16 sonu

--- src/day16_nestedForLoop/C10_NestedLoops.java
+++ src/day16_nestedForLoop/C10_NestedLoops.java
@@ -9,7 +9,7 @@ public class C10_NestedLoops {
         /*
 
         kullanicidan satir sayisini alip asagidaki sekli cizdirin
-        orn satir = 5
+        orn satir = 9
 
                 0
                 0 1
@@ -27,17 +27,33 @@ public class C10_NestedLoops {
         System.out.println("Lutfen sekildeki satir sayisini giriniz...");
         int satirSayisi = scanner.nextInt();
 
-        for (int i = 1; i <=satirSayisi ; i++) { // satirlari
-            for (int j = 1; j <=i ; j++) {
-                System.out.print(j-1 + " ");
-            }
-            System.out.println();
-        }
 
-        // 5.satira kadar her satirda satir sayisi kadar olacak sekilde ucgeni cizdik
-        // 6. satirda 4, 7.satirda 3, 8.satirda 2, 9.satirda 1, 10.satirda(2 * satir sayisi) 0
-        // olacak sekilde azaltarak 2.bolumu yazdiracak
-        // ayri bir nested for loop yazmaliyiz
+        if (satirSayisi%2 == 0){
+            System.out.println("Satir sayisi cift olamaz");
+        } else{
+
+            for (int i = 1; i <=satirSayisi/2+1 ; i++) { // satirlari
+                for (int j = 1; j <=i ; j++) {
+                    System.out.print(j-1 + " ");
+                }
+                System.out.println();
+            }
+
+            // 5.satira kadar her satirda satir sayisi kadar olacak sekilde ucgeni cizdik
+            // 6. satirda 4, 7.satirda 3, 8.satirda 2, 9.satirda 1, 10.satirda(2 * satir sayisi) 0
+            // olacak sekilde azaltarak 2.bolumu yazdiracak
+            // ayri bir nested for loop yazmaliyiz
+
+            for (int i = satirSayisi/2; i >=1 ; i--) { // satirlari
+                for (int j = 1; j <=i ; j++) {
+                    System.out.print(j-1 + " ");
+                }
+                System.out.println();
+            }
+
+
+
+        }
 
     }
 }
