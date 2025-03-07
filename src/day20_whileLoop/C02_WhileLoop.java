fedc7b449cf1f8eab93b3250daafd84e9906d500
bulutluoz <abbulutluoz@gmail.com>
2025-03-07 21:15:53 +0100

    Ders20 sonu

--- src/day20_whileLoop/C02_WhileLoop.java
+++ src/day20_whileLoop/C02_WhileLoop.java
@@ -1,5 +1,7 @@
 package day20_whileLoop;
 
+import java.util.Scanner;
+
 public class C02_WhileLoop {
 
     public static void main(String[] args) {
@@ -12,6 +14,26 @@ public class C02_WhileLoop {
         // girilen negatif sayiyi isleme dahil etmeyin
 
 
+        Scanner scanner = new Scanner(System.in);
+
+        int girilenSayi = 10;
+        int sayilarinToplami = 0 ;
+
+        while ( girilenSayi != 0 ){ // buraya loop'un bitmesi icin gerekli sarti DEGIL
+                                    // loop'un devam etmesi icin gerekli sarti yaziyoruz
+
+            System.out.println("Lutfen pozitif bir tamsayi giriniz...");
+            girilenSayi = scanner.nextInt();
+
+            if (girilenSayi > 0 ){
+                sayilarinToplami += girilenSayi;
+            } else if (girilenSayi < 0){
+                System.out.println("Pozitif sayi girmelisiniz..");
+            }
+
+        }
+
+        System.out.println("Girilen pozitif tamsayilarin toplami : " + sayilarinToplami);
 
     }
 }
