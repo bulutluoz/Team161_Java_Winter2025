cd31848f8bd64d1e198069f6239676529cc0637b
bulutluoz <abbulutluoz@gmail.com>
2025-03-07 20:21:26 +0100

    Ders20 sonu

---
+++ src/day20_whileLoop/C02_WhileLoop.java
@@ -0,0 +1,17 @@
+package day20_whileLoop;
+
+public class C02_WhileLoop {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan pozitif sayilar girmesini isteyin
+        // kullanici pozitif sayilar girdigi muddetce
+        // girilen sayilari toplayin
+        // Kullanici islemi bitirmek istediginde 0 bassin
+        // Kullanici negatif deger girerse, kullaniciya uyari yazdirin
+        // girilen negatif sayiyi isleme dahil etmeyin
+
+
+
+    }
+}
