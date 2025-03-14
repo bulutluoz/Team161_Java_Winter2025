e0568647ea13aa39b7d7b7d3a26f5225c7c7852a
bulutluoz <abbulutluoz@gmail.com>
2025-03-14 21:17:56 +0100

    25.gun sonu

---
+++ src/day25_arrays/C07_Odev.java
@@ -0,0 +1,12 @@
+package day25_arrays;
+
+public class C07_Odev {
+
+    // Kullanicidan deger alarak String bir array olusturun
+    // String array'deki en uzun ve en kisa ismi yazdirin
+
+    public static void main(String[] args) {
+
+
+    }
+}
