f9ebd074b0c15078e8bfa0b8edc0b529bf69d2b7
bulutluoz <abbulutluoz@gmail.com>
2025-03-17 20:13:56 +0100

    Day26 sonu

--- src/day25_arrays/C07_Odev.java
+++ src/day25_arrays/C07_Odev.java
@@ -7,6 +7,28 @@ public class C07_Odev {
 
     public static void main(String[] args) {
 
+        String[] kelimeler = C05_KullaniciyaArrayOlusturtma.kullaniciyaArrayOlusturt();
+
+        String enKisaKelime = kelimeler[0];
+        String enUzunKelime = kelimeler[0];
+
+        for (int i = 0; i < kelimeler.length ; i++) {
+
+            if (kelimeler[i].length() < enKisaKelime.length()){
+                // loop'un getirdigi isim en kisa isimden daha kisa
+                enKisaKelime = kelimeler[i];
+            }
+
+            if (kelimeler[i].length() > enUzunKelime.length()){
+                // loop'un getirdigi isim en uzun isimden daha uzun
+                enUzunKelime = kelimeler[i];
+            }
+
+        }
+
+        System.out.println("Girilen kelimelerden en uzun olan : " + enUzunKelime);
+        System.out.println("Girilen kelimelerden en kisa olan : " + enKisaKelime);
+
 
     }
 }
