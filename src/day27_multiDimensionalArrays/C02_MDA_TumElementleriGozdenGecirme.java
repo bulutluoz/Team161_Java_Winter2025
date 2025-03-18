a37549181cdc990a7be467138bea6027bd27f0e9
bulutluoz <abbulutluoz@gmail.com>
2025-03-18 20:12:05 +0100

    Day27 sonu

---
+++ src/day27_multiDimensionalArrays/C02_MDA_TumElementleriGozdenGecirme.java
@@ -0,0 +1,11 @@
+package day27_multiDimensionalArrays;
+
+public class C02_MDA_TumElementleriGozdenGecirme {
+
+    public static void main(String[] args) {
+
+        int[][] sayilar = {{3,5,4},{2,3,6,8},{2,4},{10},{3,9,0,2,7}};
+
+
+    }
+}
