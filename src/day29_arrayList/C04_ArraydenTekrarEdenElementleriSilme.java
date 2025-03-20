4a11d19dbf9760c070dfd68c12da137cb378870a
bulutluoz <abbulutluoz@gmail.com>
2025-03-20 20:17:27 +0100

    Day29 sonu

---
+++ src/day29_arrayList/C04_ArraydenTekrarEdenElementleriSilme.java
@@ -0,0 +1,19 @@
+package day29_arrayList;
+
+public class C04_ArraydenTekrarEdenElementleriSilme {
+
+    public static void main(String[] args) {
+
+        //  Verilen bir array’de
+        //  tekrar eden elementler icin, mukerrer olanlari silip,
+        //  tum elemanlardan sadece 1 tane yapip bize dondurecek bir method olusturun.
+
+        int[] arr = {3,5,6,8,2,3,5,1,4,8,6,2,3,4,8,1};
+
+
+
+
+
+        // arr ==> [3,5,6,8,2,1,4]
+    }
+}
