b6d2b701b39234230ab8bcf4c94f43ac49ad4ada
bulutluoz <abbulutluoz@gmail.com>
2025-03-25 21:10:16 +0100

    day32 sonu

--- src/day32_constructors/C01_Car.java
+++ src/day32_constructors/C01_Car.java
@@ -9,13 +9,31 @@ public class C01_Car {
     int fiyat;
 
 
+    C01_Car(String mrk, String mdl,int yl, int fyt, int iNo){
+
+        marka = mrk;
+        model = mdl ;
+        yil = yl;
+        fiyat = fyt;
+        ilanNo = iNo;
+    }
+
     /*
-        Bir class olusturdugumuzda
-        baska class'larda bu class'dan obje olusturuldugunda
-        objenin referansini degil
-        TUM OZELLIKLERINI yazdirmasini isterseniz
-        toString()'nu class'iniza uyarlamalisiniz
+        Biz gorunur bir constructor olusturdugumuzda
+        Java default constructor'i siler
+        Bu durumda daha once
+        default constructor ile olusturulan
+        objeler CTE verir.
+
+        CTE vermesinin onune gecmek icin
+        bir class'da GORUNUR bir constructor olusturdugumuzda
+        default constructor'in yerine
+        PARAMETRESIZ bir constructor DA olustururuz
+
      */
+    C01_Car(){
+
+    }
 
 
 
@@ -24,8 +42,13 @@ public class C01_Car {
 
 
 
-
-
+    /*
+            Bir class olusturdugumuzda
+            baska class'larda bu class'dan obje olusturuldugunda
+            objenin referansini degil
+            TUM OZELLIKLERINI yazdirmasini isterseniz
+            toString()'nu class'iniza uyarlamalisiniz
+         */
     @Override
     public String toString() {
         return "Ozellikler ==> " +
