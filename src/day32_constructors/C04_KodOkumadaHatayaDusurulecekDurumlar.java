ae2f787590fa5a2e2976b7a40747ae1a51d66fd4
bulutluoz <abbulutluoz@gmail.com>
2025-03-26 19:50:03 +0100

    day32 sonu

--- src/day32_constructors/C04_KodOkumadaHatayaDusurulecekDurumlar.java
+++ src/day32_constructors/C04_KodOkumadaHatayaDusurulecekDurumlar.java
@@ -10,8 +10,25 @@ public class C04_KodOkumadaHatayaDusurulecekDurumlar {
         isim = "Can";
     }
 
+    C04_KodOkumadaHatayaDusurulecekDurumlar(String str){
+        isim = isim.toUpperCase();
+        sayi /= 2;
+    }
+
+    C04_KodOkumadaHatayaDusurulecekDurumlar(String a, int b){
+
+        isim = a.substring(2);
+        sayi = a.length();
+    }
+
 
     public static void main(String[] args) {
+
+        C04_KodOkumadaHatayaDusurulecekDurumlar obj3 = new C04_KodOkumadaHatayaDusurulecekDurumlar("Serpil",5);
+        System.out.println(obj3.isim); // rpil
+        System.out.println(obj3.sayi); // 6
+
+
 
         C04_KodOkumadaHatayaDusurulecekDurumlar obj1 = new C04_KodOkumadaHatayaDusurulecekDurumlar();
 
@@ -29,7 +46,10 @@ public class C04_KodOkumadaHatayaDusurulecekDurumlar {
         System.out.println(obj1.isim); // Can
         System.out.println(obj1.sayi); // 20
 
+        C04_KodOkumadaHatayaDusurulecekDurumlar obj2 = new C04_KodOkumadaHatayaDusurulecekDurumlar("Veli");
 
+        System.out.println(obj2.isim); // ALI
+        System.out.println(obj2.sayi); // 15
     }
 
 
