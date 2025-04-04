79450d09959cd81576eefbaffbe834f7981245c9
bulutluoz <abbulutluoz@gmail.com>
2025-04-04 20:27:54 +0200

    ders38 sonu

--- src/day38_localDateTime/C01_LocalTime.java
+++ src/day38_localDateTime/C01_LocalTime.java
@@ -9,19 +9,34 @@ public class C01_LocalTime {
 
         LocalTime yerelSaat = LocalTime.now();
 
-        // LocalTime Class'indaki now() method'u
-        // calistigi bilgisayardaki zamani alip kodun calistigi satira getirir
+        /*
+         LocalTime Class'indaki now() method'u
+         calistigi bilgisayardaki zamani alip kodun calistigi satira getirir
 
-        System.out.println(yerelSaat); // 19:48:48.559684   19:49:04.903377
+         ANCAAAAAKKKK bu method calistigi andaki degeri bize getirir
+         bir kronometre gibi zamani tutmaya devam etmez
+
+         yerelSaat degeri yeni bir atama olmadikca degismez
+         */
+
+
+
+        System.out.println("loop'dan once yerel saat : " + yerelSaat); // 19:48:48.559684   19:49:04.903377
 
         // asagidaki islemin kac saniyede bitirildigini yazdirin
 
+        LocalTime baslangicSaati = LocalTime.now();
+        System.out.println("loop'dan once baslangicSaati : " + baslangicSaati);
+
         String str = "";
 
-        for (int i = 0; i <100000 ; i++) {
+        for (int i = 0; i <200000 ; i++) {
             str+=i;
         }
 
+        LocalTime bitisSaati = LocalTime.now();
+        System.out.println("loop'dan sonra yerel saat : " + yerelSaat);
+        System.out.println("loop'dan sonra bitisSaati : " + bitisSaati);
 
 
     }
