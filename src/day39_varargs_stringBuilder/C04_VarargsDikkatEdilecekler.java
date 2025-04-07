f57241af217d855220c35a7ed8553046399eee1e
bulutluoz <abbulutluoz@gmail.com>
2025-04-07 19:45:33 +0200

    ders38 sonu

---
+++ src/day39_varargs_stringBuilder/C04_VarargsDikkatEdilecekler.java
@@ -0,0 +1,14 @@
+package day39_varargs_stringBuilder;
+
+public class C04_VarargsDikkatEdilecekler {
+
+    public static void main(String[] args) {
+
+        // Kac tane argument yazarsak yazalim
+        // son argument haric geriye kalanlari toplayip
+        // bulunan toplam ile son argumanin carpimini yazdiran bir method olusturun
+        // orn:  2,3,4 ==>   output (2+3) * 4 ==> 20
+
+
+    }
+}
