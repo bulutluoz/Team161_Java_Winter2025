c6adc3f5d7ffeb1db84702eef8585183a5328650
bulutluoz <abbulutluoz@gmail.com>
2025-04-10 21:45:45 +0200

    ders42 sonu

--- src/day42_inheritance/C06_Child.java
+++ src/day42_inheritance/C06_Child.java
@@ -12,6 +12,19 @@ public class C06_Child extends C05_Parent{
             Child class'da olusturulan bir obje
             OTOMATIK olarak parent class'daki TUM OZELLIKLERE sahip olur
 
+            Bir obje'ye class'daki instance variabler'lar
+            ve degerlerinin bir kopyasini alip yapistirma gorevi
+            Constructor'dadir.
+
+            Yani child class'daki objenin
+            parent class'daki ozelliklere sahip olmasi icin
+            Child class constractor'i ile birlikte
+            Parent Class constructor'i da CALISMALIDIR
+
+            Bunun icin Java
+            extends keyword kullanan class'lardaki
+            her constructor'in ilk satirina
+            gizli bir super() constructor call koymustur
 
 
          */
