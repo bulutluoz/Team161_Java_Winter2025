6a142cab1804b4dc1ecfc9ba537ed6315f4bfa65
bulutluoz <abbulutluoz@gmail.com>
2025-04-11 19:29:18 +0200

    ders43 sonu

---
+++ src/day43_constructorCall_ClassUyeleriniKullanma/C03_Corolla.java
@@ -0,0 +1,16 @@
+package day43_constructorCall_ClassUyeleriniKullanma;
+
+public class C03_Corolla extends C02_Toyota {
+    C03_Corolla(){
+        System.out.println("Corolla constructor calisti");
+    }
+
+    String model = "Corolla";
+    String sanzuman = "vvti";
+    String lastik = "Pirelli Turkiye";
+
+    String aku = "Inci";
+    String renk = "Renk belirtilmedi";
+    String yakitTuru = "Yakit turu belirtilmedi";
+
+}
