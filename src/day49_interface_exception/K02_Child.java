a31b1775084d4715c1b5905aa37918a0d8471a0c
bulutluoz <abbulutluoz@gmail.com>
2025-04-22 21:53:14 +0200

    ders49 sonu

--- src/day49_interface_exception/K02_Child.java
+++ src/day49_interface_exception/K02_Child.java
@@ -3,7 +3,7 @@ package day49_interface_exception;
 import java.util.ArrayList;
 import java.util.List;
 
-public class C02_Child implements I01_Interface{
+public class K02_Child implements I01_Interface{
 
     /*
 
@@ -19,11 +19,34 @@ public class C02_Child implements I01_Interface{
         bir istisna getirmistir.
 
 
+        static veya default keyword' kullanilarak
+        interface icinde body'si olan
+        yani child class'lar tarafindan uyarlanmak zorunda olmayan
+        method'lar olusturulabilir
+
+        bu iki keyword'un farki
+        o method'lara erisim yontemindedir
      */
 
     public static void main(String[] args) {
 
         List<String> isimler = new ArrayList<>();
+
+        // static keyword kullanan istisnai method'lar
+        // interfaceIsmi.methodismi() seklinde kullanilabilir
+
+        I01_Interface.method8();
+        // Bu method static keyword kullandigi icin,
+        // interface icinde olmasina ragmen body'si vardir
+
+        // default keyword kullanan istisnai method'lar ise obje uzerinden
+        // objeIsmi.methodismi() seklinde kullanilabilir
+
+        I01_Interface obj = new K02_Child();
+        obj.method7();
+        // bu method default keyword kullandigi icin,
+        // child class'larin implement etmesi zorunlu degil
+
     }
 
 
