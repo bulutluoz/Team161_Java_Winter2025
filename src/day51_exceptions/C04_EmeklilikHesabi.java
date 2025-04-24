ac02c59a69ceec373fe97a4d5998b16154d7e919
bulutluoz <abbulutluoz@gmail.com>
2025-04-24 20:46:21 +0200

    ders51 sonu

---
+++ src/day51_exceptions/C04_EmeklilikHesabi.java
@@ -0,0 +1,18 @@
+package day51_exceptions;
+
+public class C04_EmeklilikHesabi {
+
+    public static void main(String[] args) {
+        // Kod'un herhangi bir satirinda kontrollu olarak exception firlatilabilir
+        // Kontrollu bir sekilde baska bir bolume gecis veya
+        // kullaniciya istenen mesaji iletme olabilir
+
+        // kullanicidan yasini, cinsiyetini ve uyrugunu isteyin
+        // Yasi 60 ve 75 arasi olan (sinirlar dahil)
+        // cinsiyeti E veya K
+        // uyrugu TC olanlar emekli olabilirler.
+
+        // bilgileri alirken bir bilgi istenmeyen sekilde ise
+        // hemen emekli olamazsin yazdirsin, diger bilgileri istemesin
+    }
+}
