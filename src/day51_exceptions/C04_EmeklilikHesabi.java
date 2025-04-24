444eedd0932e8dcec69260a5f9f8bcc06d4536e7
bulutluoz <abbulutluoz@gmail.com>
2025-04-24 21:51:13 +0200

    ders51 sonu

--- src/day51_exceptions/C04_EmeklilikHesabi.java
+++ src/day51_exceptions/C04_EmeklilikHesabi.java
@@ -1,5 +1,7 @@
 package day51_exceptions;
 
+import java.util.Scanner;
+
 public class C04_EmeklilikHesabi {
 
     public static void main(String[] args) {
@@ -14,5 +16,37 @@ public class C04_EmeklilikHesabi {
 
         // bilgileri alirken bir bilgi istenmeyen sekilde ise
         // hemen emekli olamazsin yazdirsin, diger bilgileri istemesin
+
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Yaşınızı giriniz: ");
+            int yas = scanner.nextInt();
+
+            if (yas < 60 || yas > 75) {
+                throw new Exception("Yaş aralığı 60 ile 75 arasında olmalıdır");
+            }
+            scanner.nextLine();
+            System.out.print("Cinsiyetinizi giriniz (E/K): ");
+            String cinsiyet = scanner.nextLine();
+            if (!(cinsiyet.equalsIgnoreCase("e") || cinsiyet.equalsIgnoreCase("K"))) {
+                throw new Exception("Cinsiyet geçersiz");
+            }
+            System.out.print("Uyruk giriniz (TC/YD): ");
+            String uyruk = scanner.next().toUpperCase();
+            if (!uyruk.equalsIgnoreCase("TC")) {
+                throw new Exception("Uyruk uygun değil");
+            }
+            System.out.println("Tebrikler! Emekli olabilirsiniz.\n" + "Yaşınız " + yas + "\n" + "Cinsiyet " + cinsiyet + "\n" + "Uyruk " + uyruk);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Kod normal olarak calismaya devam eder");
+
+
+
+
+
+
     }
 }
