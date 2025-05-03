63ee746acdccaa8bfbf8ee969f366bf0f1f19cdd
bulutluoz <abbulutluoz@gmail.com>
2025-05-03 21:42:59 +0200

    56.ders sonu

--- src/day56_nestedMaps/C03_NestedMapOlusturma.java
+++ src/day56_nestedMaps/C03_NestedMapOlusturma.java
@@ -72,26 +72,5 @@ public class C03_NestedMapOlusturma {
         System.out.println(ogrenciNestedMap);
 
 
-
-
-
-
-
-
-
-
-
-
-
-        /*
-                 ogrenciMap.put(101,"Ali-Can-11-H-MF");
-                ogrenciMap.put(102,"Veli-Cem-10-K-TM");
-                ogrenciMap.put(103,"Ali-Cem-11-K-TM");
-                ogrenciMap.put(104,"Ayse-Can-10-H-MF");
-                ogrenciMap.put(105,"Sevgi-Cem-11-M-TM");
-                ogrenciMap.put(106,"Sevgi-Can-10-K-MF");
-                ogrenciMap.put(107,"Esra-Han-11-M-SOZ");
-         */
-
     }
 }
